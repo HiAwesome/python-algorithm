5cfc8956c6a6c993905d1b4136c73f1436322fad
moqi <moqimoqidea@gmail.com>
2020-05-12 08:50:58 +0800

    20200512

--- c02/p069.py
+++ c02/p069.py
@@ -36,17 +36,17 @@ def test4():
 
 if __name__ == '__main__':
     t1 = Timer("test1()", "from __main__ import test1")
-    print("通过 += 添加元素执行 1000 次耗时", t1.timeit(number=1000), "毫秒")
+    print("通过 += 添加元素执行 1000 次耗时", t1.timeit(number=1000), "秒")
     t2 = Timer("test2()", "from __main__ import test2")
-    print("通过 append 添加元素执行 1000 次耗时", t2.timeit(number=1000), "毫秒")
+    print("通过 append 添加元素执行 1000 次耗时", t2.timeit(number=1000), "秒")
     t3 = Timer("test3()", "from __main__ import test3")
-    print("列表解析式执行 1000 次耗时", t3.timeit(number=1000), "毫秒")
+    print("列表解析式执行 1000 次耗时", t3.timeit(number=1000), "秒")
     t4 = Timer("test4()", "from __main__ import test4")
-    print("列表构造函数执行 1000 次耗时", t4.timeit(number=1000), "毫秒")
+    print("列表构造函数执行 1000 次耗时", t4.timeit(number=1000), "秒")
 
 """
-通过 += 添加元素执行 1000 次耗时 0.10332232100000001 毫秒
-通过 append 添加元素执行 1000 次耗时 0.06958381 毫秒
-列表解析式执行 1000 次耗时 0.031555849 毫秒
-列表构造函数执行 1000 次耗时 0.01182483199999998 毫秒
+通过 += 添加元素执行 1000 次耗时 0.10332232100000001 秒
+通过 append 添加元素执行 1000 次耗时 0.06958381 秒
+列表解析式执行 1000 次耗时 0.031555849 秒
+列表构造函数执行 1000 次耗时 0.01182483199999998 秒
 """
