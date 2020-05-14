4e59686f90eea76a1e46b2db7a59fe69c3401b01
moqi <moqimoqidea@gmail.com>
2020-05-14 08:41:10 +0800

    20200514

---
+++ c04/p127.py
@@ -0,0 +1,19 @@
+"""
+用海龟画一个递归螺线
+"""
+
+import turtle
+
+myTurtle = turtle.Turtle()
+myWin = turtle.Screen()
+
+
+def drawSpiral(myTurtle, lineLen):
+    if lineLen > 0:
+        myTurtle.forward(lineLen)
+    myTurtle.right(90)
+    drawSpiral(myTurtle, lineLen - 5)
+
+
+drawSpiral(myTurtle, 100)
+myWin.exitonclick()
