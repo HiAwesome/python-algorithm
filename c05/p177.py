2adcee03bc249a72be0459ab19e2b2b8ba33be13
moqi <moqimoqidea@gmail.com>
2020-05-17 21:58:09 +0800

    20200517

---
+++ c05/p177.py
@@ -0,0 +1,20 @@
+def selectionSort(alist):
+    for fillslot in range(len(alist) - 1, 0, -1):
+        positionOfMax = 0
+        for location in range(1, fillslot + 1):
+            if alist[location] > alist[positionOfMax]:
+                positionOfMax = location
+
+        alist[fillslot], alist[positionOfMax] = alist[positionOfMax], alist[fillslot]
+
+
+if __name__ == '__main__':
+    alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    print(alist)
+    selectionSort(alist)
+    print(alist)
+
+"""
+[54, 26, 93, 17, 77, 31, 44, 55, 20]
+[17, 20, 26, 31, 44, 54, 55, 77, 93]
+"""
