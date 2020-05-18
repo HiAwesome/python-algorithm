bb9cd1780adbe3f3cdb213016bbc69e5ad02d6b3
moqi <moqimoqidea@gmail.com>
2020-05-18 22:08:43 +0800

    20200518

--- c06/p206.py
+++ c06/p206.py
@@ -1,3 +1,5 @@
+import operator
+
 from pythonds.basic.stack import Stack
 from pythonds.trees.binaryTree import BinaryTree
 
@@ -31,15 +33,37 @@ def buildParseTree(fpexp):
     return eTree
 
 
+def evaluate(parseTree):
+    opers = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv
+    }
+
+    leftC = parseTree.getLeftChild()
+    rightC = parseTree.getRightChild()
+
+    if leftC and rightC:
+        fn = opers[parseTree.getRootVal()]
+        return fn(evaluate(leftC), evaluate(rightC))
+    else:
+        return parseTree.getRootVal()
+
+
 if __name__ == '__main__':
     # 需要空格夹杂以备切割，如果去掉此操作则多位数解析为多个单位数，会出错
     pt = buildParseTree('( ( 10 + 5 ) * 3 )')
     pt.postorder()
 
+    print()
+    print('result is %d' % evaluate(pt))
 """
 10
 5
 +
 3
 *
+
+result is 45
 """
