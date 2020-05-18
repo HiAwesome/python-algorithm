d0c962ef4411d064f46a84aedc73ba42231f09fe
moqi <moqimoqidea@gmail.com>
2020-05-18 08:45:27 +0800

    20200518

--- c06/p193.py
+++ c06/p193.py
@@ -9,8 +9,15 @@ myTree = [
      []]
 ]
 
-print(myTree)
+if __name__ == '__main__':
+    print(myTree)
+    print('left subtree = %s' % myTree[1])
+    print('root = %s' % myTree[0])
+    print('right subtree = %s' % myTree[2])
 
 """
 ['a', ['b', ['d', [], []], ['e', [], []]], ['c', ['f', [], []], []]]
+left subtree = ['b', ['d', [], []], ['e', [], []]]
+root = a
+right subtree = ['c', ['f', [], []], []]
 """
