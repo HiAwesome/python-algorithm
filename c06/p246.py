b4eeb14e85acb421a69ad6382c7de58e2a6a770b
moqi <moqimoqidea@gmail.com>
2020-05-19 09:14:57 +0800

    20200519

--- c06/p246.py
+++ c06/p246.py
@@ -28,3 +28,61 @@ class AVLTree(BinarySearchTree):
 
         if node.parent.balanceFactor != 0:
             self.updateBalance(node.parent)
+
+    def rebalance(self, node: TreeNode):
+        if node.balanceFactor < 0:
+            if node.rightChild.balanceFactor > 0:
+                self.rotateRight(node.rightChild)
+                self.rotateLeft(node)
+            else:
+                self.rotateLeft(node)
+        elif node.balanceFactor > 0:
+            if node.leftChild.balanceFactor < 0:
+                self.rotateLeft(node.leftChild)
+                self.rotateRight(node)
+            else:
+                self.rotateRight(node)
+
+    def rotateLeft(self, rotRoot: TreeNode):
+        newRoot = rotRoot.rightChild
+        rotRoot.rightChild = newRoot.leftChild
+
+        if newRoot.leftChild is not None:
+            newRoot.leftChild.parent = rotRoot
+
+        newRoot.parent = rotRoot.parent
+
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isLeftChild():
+                rotRoot.parent.leftChild = newRoot
+            else:
+                rotRoot.parent.rightChild = newRoot
+
+        newRoot.leftChild = rotRoot
+        rotRoot.parent = newRoot
+        rotRoot.balanceFactor = rotRoot.balanceFactor + 1 - min(newRoot.balanceFactor, 0)
+        newRoot.balanceFactor = newRoot.balanceFactor + 1 + max(rotRoot.balanceFactor, 0)
+
+    def rotateRight(self, rotRoot: TreeNode):
+        newRoot = rotRoot.leftChild
+        rotRoot.leftChild = newRoot.rightChild
+
+        if newRoot.rightChild is not None:
+            newRoot.rightChild.parent = rotRoot
+
+        newRoot.parent = rotRoot.parent
+
+        if rotRoot.isRoot():
+            self.root = newRoot
+        else:
+            if rotRoot.isRightChild():
+                rotRoot.parent.rightChild = newRoot
+            else:
+                rotRoot.parent.leftChild = newRoot
+
+        newRoot.rightChild = rotRoot
+        rotRoot.parent = newRoot
+        rotRoot.balanceFactor = rotRoot.balanceFactor - 1 - max(newRoot.balanceFactor, 0)
+        newRoot.balanceFactor = newRoot.balanceFactor - 1 + min(rotRoot.balanceFactor, 0)
