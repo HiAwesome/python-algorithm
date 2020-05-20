1a9448bc04cd64bad7d816016757b50182fa4f94
moqi <moqimoqidea@gmail.com>
2020-05-20 09:43:09 +0800

    20200520

--- c07/p270.py
+++ c07/p270.py
@@ -1,4 +1,4 @@
-from pythonds.graphs import Graph
+from pythonds.graphs import Graph, Vertex
 
 
 def knightGraph(bdSize):
@@ -36,3 +36,40 @@ def genLegalMoves(x, y, bdSize):
 
 def legalCoord(x, bdSize):
     return True if 0 <= x < bdSize else False
+
+
+def knightTour(n, path, u: Vertex, limit):
+    u.setColor('gray')
+    path.append(u)
+
+    if n < limit:
+        nbrList = list(u.getConnections())
+        i = 0
+        done = False
+        while i < len(nbrList) and not done:
+            if nbrList[i].getColor() == 'white':
+                done = knightTour(n + 1, path, nbrList[i], limit)
+            i += 1
+
+        if not done:  # prepare to backtrack
+            path.pop()
+            u.setColor('white')
+        else:
+            done = True
+
+        return done
+
+
+def orderByAvail(n: Vertex):
+    resList = []
+    for v in n.getConnections():
+        if v.getColor() == 'white':
+            c = 0
+            for w in v.getConnections():
+                if w.getColor == 'white':
+                    c += 1
+
+            resList.append((c, v))
+
+    resList.sort(key=lambda x: x[0])
+    return [y[1] for y in resList]
