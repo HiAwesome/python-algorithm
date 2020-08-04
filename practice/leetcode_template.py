c0d8a458da24c5841506bc6999a887775977aa76
moqi <moqimoqidea@gmail.com>
2020-08-04 09:49:09 +0800

    20200804

--- practice/leetcode_template.py
+++ practice/leetcode_template.py
@@ -2,28 +2,29 @@ from typing import List
 
 
 class Solution:
-    def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        def possible(guess) -> bool:
-            count, left = 0, 0
-            for right, x in enumerate(nums):
-                while x - nums[left] > guess:
-                    left += 1
-                count += right - left
-            return count >= k
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        dic = {}
+        for index, value in enumerate(list1):
+            dic[value] = index
 
-        nums.sort()
-        lo, hi = 0, nums[-1] - nums[0]
+        res = []
 
-        while lo < hi:
-            mi = lo + (hi - lo) // 2
-            if possible(mi):
-                hi = mi
-            else:
-                lo = mi + 1
+        min_sum = float('inf')
 
-        return lo
+        for index, value in enumerate(list2):
+            if index <= min_sum:
+                if list2[index] in dic:
+                    sum1 = index + dic[list2[index]]
+                    if sum1 < min_sum:
+                        res.clear()
+                        res.append(list2[index])
+                        min_sum = sum1
+                    elif sum1 == min_sum:
+                        res.append(list2[index])
+
+        return res
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.smallestDistancePair([4, 5, 6, 7, 0, 1, 2], 0))
+    print(s.findRestaurant(['tom', 'nancy'], ['jack', 'tom', 'nancy']))
