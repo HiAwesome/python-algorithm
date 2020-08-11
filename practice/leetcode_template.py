1988e2afa37298531a2712879151b930eed35eec
moqi <moqimoqidea@gmail.com>
2020-08-11 09:49:38 +0800

    20200811

--- practice/leetcode_template.py
+++ practice/leetcode_template.py
@@ -1,16 +1,43 @@
-import collections
 from typing import List
 
 
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = collections.defaultdict(list)
-        for s in strs:
-            ans[tuple(sorted(s))].append(s)
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
-        return list(ans.values())
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        def check(x) -> bool:
+            total, cnt = 0, 1
+            for num in nums:
+                if total + num > x:
+                    cnt += 1
+                    total = num
+                else:
+                    total += num
+            return cnt <= m
+
+        left, right = max(nums), sum(nums)
+
+        while left < right:
+            mid = left + (right - left) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    print(s.splitArray([7,2,5,10,8], 2))
