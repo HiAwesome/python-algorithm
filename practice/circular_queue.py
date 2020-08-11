133454cff8f602fc5f34325cae8c47feaef232db
moqi <moqimoqidea@gmail.com>
2020-08-11 09:59:52 +0800

    20200811

---
+++ practice/circular_queue.py
@@ -0,0 +1,41 @@
+"""
+https://leetcode-cn.com/problems/design-circular-queue/solution/she-ji-xun-huan-dui-lie-by-leetcode/
+"""
+
+class MyCircularQueue:
+
+    def __init__(self, capacity: int):
+        self.queue = [0] * capacity
+        self.headIndex = 0
+        self.size = 0
+        self.capacity = capacity
+
+    def enQueue(self, value: int) -> bool:
+        if self.size == self.capacity:
+            return False
+        self.queue[(self.headIndex + self.size) % self.capacity] = value
+        self.size += 1
+        return True
+
+    def deQueue(self) -> bool:
+        if self.size == 0:
+            return False
+        self.headIndex = (self.headIndex + 1) % self.capacity
+        self.size -= 1
+        return True
+
+    def Front(self) -> int:
+        if self.size == 0:
+            return  -1
+        return self.queue[self.headIndex]
+
+    def Rear(self) -> int:
+        if self.size == 0:
+            return -1
+        return self.queue[(self.headIndex + self.size - 1) % self.capacity]
+
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
