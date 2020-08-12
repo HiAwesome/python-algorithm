d7d58477e35abde78b2ce4a7c7e8dfd035d65389
moqi <moqimoqidea@gmail.com>
2020-08-12 20:15:32 +0800

    20200812

---
+++ practice/implement_stack_using_queues.py
@@ -0,0 +1,31 @@
+"""
+https://leetcode-cn.com/problems/implement-stack-using-queues/solution/python3-list-dequeshi-xian-by-xilepeng/
+"""
+from collections import deque
+
+
+class MyStack:
+    def __init__(self):
+        self.data = deque()
+        self.help = deque()
+
+    def push(self, x: int) -> None:
+        self.data.append(x)
+
+    def pop(self) -> int:
+        while len(self.data) > 1:
+            self.help.append(self.data.popleft())
+        tmp = self.data.popleft()
+        self.help, self.data = self.data, self.help
+        return tmp
+
+    def top(self) -> int:
+        while len(self.data) > 1:
+            self.help.append(self.data.popleft())
+        tmp = self.data.popleft()
+        self.help.append(tmp)
+        self.help, self.data = self.data, self.help
+        return tmp
+
+    def empty(self) -> bool:
+        return not self.data
