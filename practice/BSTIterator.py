62ee89aa318468ded91c57865ce9ed63a2a0d458
moqi <moqimoqidea@gmail.com>
2020-08-14 09:35:39 +0800

    20200814

--- practice/BSTIterator.py
+++ practice/BSTIterator.py
@@ -2,8 +2,8 @@
 https://leetcode-cn.com/problems/binary-search-tree-iterator/solution/er-cha-sou-suo-shu-die-dai-qi-by-leetcode/
 """
 
-
-class BSTIterator:
+# 中序遍历，初始化时完成遍历，空间复杂度 O(n)
+class BSTIteratorA:
 
     def __init__(self, root: TreeNode):
         self.node_array = []
@@ -29,3 +29,30 @@ class BSTIterator:
         @return whether we have a next smallest number
         """
         return self.index + 1 < len(self.node_array)
+
+# 中序遍历，受控递归，next 方法时不断更新元素，空间复杂度 O(h)
+class BSTIteratorB:
+
+    def __init__(self, root: TreeNode):
+        self.stack = []
+        self._leftmost_inorder(root)
+
+    def _leftmost_inorder(self, node):
+        while node:
+            self.stack.append(node)
+            node = node.left
+
+    def next(self) -> int:
+        """
+        @return the next smallest number
+        """
+        topmost_node = self.stack.pop()
+        if topmost_node.right:
+            self._leftmost_inorder(topmost_node.right)
+        return topmost_node.val
+
+    def hasNext(self) -> bool:
+        """
+        @return whether we have a next smallest number
+        """
+        return not self.stack
