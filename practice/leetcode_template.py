78d78de44bdb6977cf72deaf24d4b5a466b04ca2
moqi <moqimoqidea@gmail.com>
2020-08-18 10:15:36 +0800

    20200818 212.单词搜索 II

--- practice/leetcode_template.py
+++ practice/leetcode_template.py
@@ -14,31 +14,29 @@ class TreeNode:
         self.right = None
 
 
-from sortedcontainers import SortedSet
-
-
 class Solution:
-    def containsNearbyAlmostDuplicate(self, nums: List[int], k: int, t: int) -> bool:
-        sorted_set = SortedSet()
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        trie = {}  # 构造字典树
+        for word in words:
+            node = trie
+            for char in word:
+                node = node.setdefault(char, {})
+            node['#'] = True
 
-        for i in range(len(nums)):
-            num = nums[i]
+        def search(i, j, node, pre, visited):  # (i,j)当前坐标，node当前trie树结点，pre前面的字符串，visited已访问坐标
+            if '#' in node:  # 已有字典树结束
+                res.add(pre)  # 添加答案
+            for (di, dj) in ((-1, 0), (1, 0), (0, -1), (0, 1)):
+                _i, _j = i + di, j + dj
+                if -1 < _i < h and -1 < _j < w and board[_i][_j] in node and (_i, _j) not in visited:  # 可继续搜索
+                    search(_i, _j, node[board[_i][_j]], pre + board[_i][_j], visited | {(_i, _j)})  # dfs搜索
 
-            # find the successor of current element
-            if sorted_set and sorted_set.bisect_left(num) < len(sorted_set):
-                if sorted_set[sorted_set.bisect_left(num)] <= num + t:
-                    return True
-
-            # find the predecessor of current element
-            if sorted_set and sorted_set.bisect_left(num) != 0:
-                if num <= sorted_set[sorted_set.bisect_left(num) - 1] + t:
-                    return True
-
-            sorted_set.add(num)
-            if len(sorted_set) > k:
-                sorted_set.remove(nums[i - k])
-
-        return False
+        res, h, w = set(), len(board), len(board[0])
+        for i in range(h):
+            for j in range(w):
+                if board[i][j] in trie:  # 可继续搜索
+                    search(i, j, trie[board[i][j]], board[i][j], {(i, j)})  # dfs搜索
+        return list(res)
 
 
 if __name__ == '__main__':
