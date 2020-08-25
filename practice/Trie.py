70e2760de89e0a103fe082f8a4fa6e64ee7a1fea
moqi <moqimoqidea@gmail.com>
2020-08-25 22:02:48 +0800

    20200825

--- practice/Trie.py
+++ practice/Trie.py
@@ -1,50 +1,7 @@
 """
-https://leetcode-cn.com/problems/implement-trie-prefix-tree/solution/184-ms-faster-than-6149-of-python3-by-yaosw/
+实现前缀树（Trie）
+https://leetcode-cn.com/problems/implement-trie-prefix-tree/
 """
-
-from collections import defaultdict
-
-
-class TireNode:
-    def __init__(self):
-        # dict的value是TireNode对象而不是字符
-        self.node = defaultdict(TireNode)
-        self.isEnd = False
-
-
-class Trie:
-    def __init__(self):
-        self.root = TireNode()
-
-    def insert(self, word: str) -> None:
-        cur = self.root
-
-        for w in word:
-            cur = cur.node[w]
-
-        cur.isEnd = True
-
-    def search(self, word: str) -> bool:
-        cur = self.root
-
-        for w in word:
-            if w in cur.node:
-                cur = cur.node[w]
-            else:
-                return False
-
-        return cur.isEnd
-
-    def startsWith(self, prefix: str) -> bool:
-        cur = self.root
-
-        for p in prefix:
-            if p in cur.node:
-                cur = cur.node[p]
-            else:
-                return False
-
-        return True
 
 
 class Trie:
@@ -59,7 +16,6 @@ class Trie:
                 t[c] = {}
             t = t[c]
 
-        # 这里的'-'就相当于前面声明的isEnd, 用来标记是不是结束了, 因为是用字典实现的所以可以随便声明key去标记
         t["-"] = True
 
     def search(self, word):
