19c7b7a4a5c64f42d6e16e29efbfafcc46c507d2
moqi <moqimoqidea@gmail.com>
2020-08-25 21:07:01 +0800

    20200825

--- practice/lru_cache2.py
+++ practice/lru_cache2.py
@@ -8,17 +8,14 @@ class Node:
     def __init__(self, key=0, value=0):
         self.key = key
         self.value = value
-        self.prev = None
-        self.next = None
+        self.next, self.prev = None, None
 
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = dict()
-        # 使用伪头部和伪尾部节点
-        self.head = Node()
-        self.tail = Node()
+        self.cache = {}
+        self.head, self.tail = Node(), Node()
         self.head.next = self.tail
         self.tail.prev = self.head
         self.capacity = capacity
@@ -27,28 +24,23 @@ class LRUCache:
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        # 如果 key 存在，先通过哈希表定位，再移到头部
+
         node = self.cache[key]
         self.move_to_head(node)
         return node.value
 
     def put(self, key: int, value: int) -> None:
         if key not in self.cache:
-            # 如果 key 不存在，创建一个新的节点
             node = Node(key, value)
-            # 添加进哈希表
             self.cache[key] = node
-            # 添加至双向链表的头部
             self.add_to_head(node)
             self.size += 1
+
             if self.size > self.capacity:
-                # 如果超出容量，删除双向链表的尾部节点
                 removed = self.remove_tail()
-                # 删除哈希表中对应的项
                 self.cache.pop(removed.key)
                 self.size -= 1
         else:
-            # 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
             node = self.cache[key]
             node.value = value
             self.move_to_head(node)
