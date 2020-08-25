9e3df9e8aa79de7de537349ffef790e419dd5f15
moqi <moqimoqidea@gmail.com>
2020-08-25 19:55:05 +0800

    20200825

--- practice/doubly_linked_list.py
+++ practice/doubly_linked_list.py
@@ -1,5 +1,8 @@
 """
-https://leetcode-cn.com/problems/design-linked-list/solution/she-ji-lian-biao-by-leetcode/
+设计链表
+https://leetcode-cn.com/problems/design-linked-list/
+
+双向链表
 """
 
 
--- practice/singly_linked_list.py
+++ practice/singly_linked_list.py
@@ -1,5 +1,8 @@
 """
-https://leetcode-cn.com/problems/design-linked-list/solution/she-ji-lian-biao-by-leetcode/
+设计链表
+https://leetcode-cn.com/problems/design-linked-list/
+
+单向链表
 """
 
 
