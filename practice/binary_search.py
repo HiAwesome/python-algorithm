5106eaac12fd717b68c95c3734163fab8230cc41
moqi <moqimoqidea@gmail.com>
2020-08-25 10:07:13 +0800

    20200825

--- practice/binary_search.py
+++ practice/binary_search.py
@@ -1,19 +1,23 @@
-def binary_search(a_list, item):
-    left, right = 0, len(a_list) - 1
-    found = False
+# https://leetcode-cn.com/problems/binary-search/
+def binary_search(nums, target):
+    if not nums:
+        return -1
 
-    while left <= right and not found:
-        mid = left + (right - left) // 2
+    if nums[0] > target or nums[-1] < target:
+        return -1
 
-        if a_list[mid] == item:
-            found = True
-        elif a_list[mid] < item:
-            left = mid + 1
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        m = l + (r - l) // 2
+        if nums[m] == target:
+            return m
+        elif nums[m] > target:
+            r = m - 1
         else:
-            right = mid - 1
+            l = m + 1
 
-    return found
-
+    return -1
 
 if __name__ == '__main__':
     print(binary_search([1, 2, 3, 4, 5], 5))
