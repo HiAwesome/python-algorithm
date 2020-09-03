e99f328b974329d2a4b8cbb8d2efa8e17591d193
moqi <moqimoqidea@gmail.com>
2020-09-03 09:59:23 +0800

    20200903

--- practice/circular_queue.py
+++ practice/circular_queue.py
@@ -12,32 +12,32 @@ class MyCircularQueue:
         self.size = 0
         self.capacity = k
 
+    def isEmpty(self) -> bool:
+        return self.size == 0
+
+    def isFull(self) -> bool:
+        return self.size == self.capacity
+
     def enQueue(self, value: int) -> bool:
-        if self.size == self.capacity:
+        if self.isFull():
             return False
         self.queue[(self.headIndex + self.size) % self.capacity] = value
         self.size += 1
         return True
 
     def deQueue(self) -> bool:
-        if self.size == 0:
+        if self.isEmpty():
             return False
         self.headIndex = (self.headIndex + 1) % self.capacity
         self.size -= 1
         return True
 
     def Front(self) -> int:
-        if self.size == 0:
+        if self.isEmpty():
             return -1
         return self.queue[self.headIndex]
 
     def Rear(self) -> int:
-        if self.size == 0:
+        if self.isEmpty():
             return -1
         return self.queue[(self.headIndex + self.size - 1) % self.capacity]
-
-    def isEmpty(self) -> bool:
-        return self.size == 0
-
-    def isFull(self) -> bool:
-        return self.size == self.capacity
