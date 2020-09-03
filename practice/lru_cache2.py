f9fa1894290b4d6f04f846ac07fa18389363562f
moqi <moqimoqidea@gmail.com>
2020-09-03 15:36:47 +0800

    20200903

--- practice/lru_cache2.py
+++ practice/lru_cache2.py
@@ -1,65 +1,60 @@
 """
 LRU Cache
 https://leetcode-cn.com/problems/lru-cache/
+
+实现
+https://leetcode-cn.com/problems/lru-cache/solution/shu-ju-jie-gou-fen-xi-python-ha-xi-shuang-xiang-li/
 """
 
 
 class Node:
-    def __init__(self, key=0, value=0):
+    def __init__(self, key=None, value=None):
         self.key = key
         self.value = value
-        self.next, self.prev = None, None
+        self.prev = None
+        self.next = None
 
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.cache = {}
-        self.head, self.tail = Node(), Node()
+        self.capacity = capacity
+        self.dic = {}
+        self.head = Node()
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.capacity = capacity
-        self.size = 0
 
-    def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-
-        node = self.cache[key]
-        self.move_to_head(node)
-        return node.value
-
-    def put(self, key: int, value: int) -> None:
-        if key not in self.cache:
-            node = Node(key, value)
-            self.cache[key] = node
-            self.add_to_head(node)
-            self.size += 1
-
-            if self.size > self.capacity:
-                removed = self.remove_tail()
-                self.cache.pop(removed.key)
-                self.size -= 1
-        else:
-            node = self.cache[key]
-            node.value = value
-            self.move_to_head(node)
-
-    def add_to_head(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
-
-    def remove_node(self, node):
+    def move_to_end(self, key):
+        node = self.dic[key]
         node.prev.next = node.next
         node.next.prev = node.prev
 
-    def move_to_head(self, node):
-        self.remove_node(node)
-        self.add_to_head(node)
+        node.prev = self.tail.prev
+        node.next = self.tail
+        self.tail.prev.next = node
+        self.tail.prev = node
 
-    def remove_tail(self):
-        node = self.tail.prev
-        self.remove_node(node)
-        return node
+    def get(self, key: int) -> int:
+        if key in self.dic:
+            self.move_to_end(key)
+        res = self.dic.get(key, -1)
+        if res == -1:
+            return res
+        else:
+            return res.value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dic:
+            self.dic[key].value = value
+            self.move_to_end(key)
+        else:
+            if len(self.dic) == self.capacity:
+                self.dic.pop(self.head.next.key)
+                self.head.next = self.head.next.next
+                self.head.next.prev = self.head
+            new = Node(key, value)
+            self.dic[key] = new
+            new.prev = self.tail.prev
+            new.next = self.tail
+            self.tail.prev.next = new
+            self.tail.prev = new
